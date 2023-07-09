7dd1b2e6ab1e97569b2f6220902d65ea58ed18f2
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-09 19:26:37 +0200

    added feedback options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import s from './App.module.css';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+}
+onLeaveFeedback = state => {
+  this.setState(prevState => ({
+    [state]: prevState[state] + 1,
+  }));
 };
